daa2c716cf0a629c0e1d7c64415cbd2a3372242e
Andy Lathrop <alathrop@blue-granite.com>
2016-02-03 09:46:30 -0700

    morning 3 FEB commit. changed variable names

--- HoltWinters-example.R
+++ HoltWinters-example.R
@@ -3,15 +3,28 @@
 
 # this code designed for RStudio
 
+# there are 2 sections to view different time series
+# manipulations and plotting: 1. basic and 2. ggplot
+
+### package info
+# ensure package 'pacman' for package management is installed
+if (!require("pacman")) install.packages("pacman")
+
+# Packages: "TTR" for time series, "forecast" for forecasting, ggplot2 for plotting
+pacman::p_load(TTR, forecast, ggplot2)
+
+### load data
+# initial data are a vector of sales values
 souvenir <- scan("http://robjhyndman.com/tsdldata/data/fancy.dat")
 
-souvenirtimeseries <- ts(souvenir, frequency=12, start=c(1987,1))
-souvenirtimeseries
-
-plot.ts(souvenirtimeseries)
+### basic wrangling, plotting, and forecasting
+# uses 'TTR' and 'forecast' package
+# convert data to ts (time series) object
+souvenir.ts <- ts(souvenir, frequency=12, start=c(1987,1))
+souvenir.ts
+plot.ts(souvenir.ts)
 
 # additive model = fluctuations in the data are roughly constant in size over time.
-
 # In this case, it appears that an additive model is not appropriate 
 # for describing this time series, since the size of the seasonal 
 # fluctuations and random fluctuations seem to increase with the level 
@@ -20,22 +33,18 @@ plot.ts(souvenirtimeseries)
 # an additive model. For example, we can transform the time series by 
 # calculating the natural log of the original data:
 
-logsouvenirtimeseries <- log(souvenirtimeseries)
-plot.ts(logsouvenirtimeseries)
-
-# ensure package 'TTR' for time series analysis is installed
-if(!require("TTR")){
-  install.packages("TTR")
-  require("TTR")
-}
+logsouvenir.ts <- log(souvenir.ts)
+plot.ts(logsouvenir.ts)
 
 # simple moving average, order 3
-logsouvenirSMA3 <- SMA(logsouvenirtimeseries,n=3)
-plot.ts(logsouvenirSMA3)
+logsouvenir.SMA3 <- SMA(logsouvenir.ts,n=3)
+plot.ts(logsouvenir.SMA3)
 
-# simple moving average, order 8
-logsouvenirSMA8 <- SMA(logsouvenirtimeseries,n=8)
-plot.ts(logsouvenirSMA8)
+# plot simple moving average (order 8), with original logsouvenir overlay 
+logsouvenir.SMA8 <- SMA(logsouvenir.ts,n=8)
+plot.ts(logsouvenir.SMA8, type="l", col="red")
+lines(logsouvenir.ts, col="black")
+points(logsouvenir.SMA8, col="red")
 
 # estimate the trend, seasonal and irregular components of this time series
 logsouvenirTScomponents <- decompose(logsouvenirtimeseries)
